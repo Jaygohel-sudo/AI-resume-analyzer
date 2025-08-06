612b9be1cd8def5e0f987d6a168138a10144e918
Jay Gohel <jrgohel14@gmail.com>
2025-08-07 01:50:07 +0530

    Implement wipe functionality

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ import path from "path";
 export default [index("routes/home.tsx"),
     route('/auth' ,'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
-    route('/resume/:id', 'routes/resume.tsx')
+    route('/resume/:id', 'routes/resume.tsx'),
+    route('/wipe', 'routes/wipe.tsx')
 ] satisfies RouteConfig;
